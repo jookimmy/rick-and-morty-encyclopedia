b33b9b83dd626d33d5e99dba8dc9a8ec2011d127
jookimmy <30672093+jookimmy@users.noreply.github.com>
2018-04-21 21:47:53 -0500

    was able to create a new page

---
+++ app/src/main/java/nojoo/com/rickandmortyencyclopedia/AllActivity.java
@@ -0,0 +1,6 @@
+package nojoo.com.rickandmortyencyclopedia;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class AllActivity extends AppCompatActivity {
+}
--- app/src/main/java/nojoo/com/rickandmortyencyclopedia/MainActivity.java
+++ app/src/main/java/nojoo/com/rickandmortyencyclopedia/MainActivity.java
@@ -1,7 +1,10 @@
 package nojoo.com.rickandmortyencyclopedia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AllActivity.class));
+            }
+        });
     }
+
 }
