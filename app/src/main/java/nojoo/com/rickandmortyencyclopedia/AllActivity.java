b7f11396fba05a73ef89fa0fbdbeaa13d40b86b2
Noah Sung <noahcs2@illinois.edu>
2018-04-24 10:36:08 -0500

    Added xml sheet for all activity page

--- app/src/main/java/nojoo/com/rickandmortyencyclopedia/AllActivity.java
+++ app/src/main/java/nojoo/com/rickandmortyencyclopedia/AllActivity.java
@@ -1,6 +1,14 @@
 package nojoo.com.rickandmortyencyclopedia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AllActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all);
+    }
 }
