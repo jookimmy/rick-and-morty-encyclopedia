e0fe122230222cabd980a034b4f50e6ad32320c3
jookimmy <30672093+jookimmy@users.noreply.github.com>
2018-04-30 14:36:01 -0500

    added background image and new text title

--- app/src/main/java/nojoo/com/rickandmortyencyclopedia/LocationClass.java
+++
@@ -1,4 +0,0 @@
-package nojoo.com.rickandmortyencyclopedia;
-
-public class LocationClass {
-}
--- app/src/main/java/nojoo/com/rickandmortyencyclopedia/MainActivity.java
+++ app/src/main/java/nojoo/com/rickandmortyencyclopedia/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button btn = findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, AllActivity.class));
             }
@@ -22,31 +23,34 @@ public class MainActivity extends AppCompatActivity {
 
         Button btn2 = findViewById(R.id.button2);
         btn2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+            @Override
+            public void onClick(View v2) {
                 startActivity(new Intent(MainActivity.this, LocationActivity.class));
             }
         });
 
         Button btn3 = findViewById(R.id.button3);
         btn3.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+            @Override
+            public void onClick(View v3) {
                 startActivity(new Intent(MainActivity.this, GenderActivity.class));
             }
         });
 
         Button btn4 = findViewById(R.id.button4);
         btn4.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+            @Override
+            public void onClick(View v4) {
                 startActivity(new Intent(MainActivity.this, StatusActivity.class));
             }
         });
 
         Button btn5 = findViewById(R.id.button5);
         btn5.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+            @Override
+            public void onClick(View v5) {
                 startActivity(new Intent(MainActivity.this, SpeciesActivity.class));
             }
         });
     }
-
 }
